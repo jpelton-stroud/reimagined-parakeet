05d090fb587a1d9666e5e5646cbed873b8ef7c8c
JPStroud <jpstroud@gmail.com>
2021-02-22 14:25:52 -0500

    init

---
+++ functions/src/datastore-interfaces.ts
@@ -0,0 +1,46 @@
+export interface Legislature extends Metadata {
+    name: string;
+    unicameral: boolean;
+    sessionLength: number;
+    chamber?: {
+        upper: string;
+        lower: string;
+    }
+    api: {
+        key: string;
+        baseURI: string;
+        memberEndpoint: string;
+        billEndpoint: string;
+    }
+}
+
+export interface Legislator extends Metadata {
+    name: {
+        first: string;
+        middle?: string;
+        last: string;
+        suffix?: string;
+    };
+    chamber?: string;
+    district: number;
+    imageURL: null | string;
+}
+
+export interface Bill extends Metadata {
+    name: {
+        short: string;
+        full: string;
+    }
+    session: number;
+    cosponsors: {
+        sponsorId: string;
+        primary: boolean;
+        version: number;
+        date: string;
+    }[]
+}
+
+interface Metadata {
+    identifiers: [];
+    updated: string;
+}
