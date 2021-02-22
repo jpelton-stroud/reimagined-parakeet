ead0de3c791c9076cbde5c7ddea565bfb38b0ea5
JPStroud <jpstroud@gmail.com>
2021-02-22 15:31:26 -0500

    update

--- functions/src/datastore-interfaces.ts
+++ functions/src/datastore-interfaces.ts
@@ -1,46 +1,49 @@
 export interface Legislature extends Metadata {
-    name: string;
-    unicameral: boolean;
-    sessionLength: number;
-    chamber?: {
-        upper: string;
-        lower: string;
-    }
-    api: {
-        key: string;
-        baseURI: string;
-        memberEndpoint: string;
-        billEndpoint: string;
-    }
+  name: {
+    short: string;
+    full: string;
+  };
+  unicameral: boolean;
+  sessionLength: number;
+  chamber?: {
+    upper: string;
+    lower: string;
+  };
+  api: {
+    key: string;
+    baseURI: string;
+    memberEndpoint: string;
+    billEndpoint: string;
+  };
 }
 
 export interface Legislator extends Metadata {
-    name: {
-        first: string;
-        middle?: string;
-        last: string;
-        suffix?: string;
-    };
-    chamber?: string;
-    district: number;
-    imageURL: null | string;
+  name: {
+    first: string;
+    middle?: string;
+    last: string;
+    suffix?: string;
+  };
+  chamber?: string;
+  district: number;
+  imageURL: null | string;
 }
 
 export interface Bill extends Metadata {
-    name: {
-        short: string;
-        full: string;
-    }
-    session: number;
-    cosponsors: {
-        sponsorId: string;
-        primary: boolean;
-        version: number;
-        date: string;
-    }[]
+  name: {
+    short: string;
+    full: string;
+  };
+  session: number;
+  cosponsors: {
+    sponsorId: string;
+    primary: boolean;
+    version: number;
+    date: string;
+  }[];
 }
 
 interface Metadata {
-    identifiers: [];
-    updated: string;
+  identifiers: [];
+  updated: string;
 }
