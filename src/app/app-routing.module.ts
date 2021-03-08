0bb76f3c712b3e6d344a1e4a107c7bf71ea903c6
JPStroud <jpstroud@gmail.com>
2021-03-08 15:37:46 -0500

    implement legislation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LegislatureComponent } from './legislature/legislature.component';
+import { LegislationComponent } from './legislation/legislation.component';
 
-const routes: Routes = [{ path: '', component: LegislatureComponent }];
+const routes: Routes = [
+  { path: '', component: LegislatureComponent },
+  { path: 'bills/:billId', component: LegislationComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/legislation/legislation.component.ts
+++ src/app/legislation/legislation.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface test {
+  name: string;
+  identifier: string;
+  sponsors: [];
+}
 
 @Component({
   selector: 'app-legislation',
   templateUrl: './legislation.component.html',
-  styleUrls: ['./legislation.component.sass']
+  styleUrls: ['./legislation.component.sass'],
 })
 export class LegislationComponent implements OnInit {
+  public legislationCollection$: Observable<test[]>;
+  public bill: test = {
+    name: `bill number not found.
+      If your internet connection is slow it may show up in a sec, 
+      otherwise please check the bill number & try again`,
+    identifier: '',
+    sponsors: [],
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public readonly db: AngularFirestore,
+    private route: ActivatedRoute
+  ) {
+    this.legislationCollection$ = this.db
+      .collection<test>('legislation', (ref) =>
+        ref.where(
+          'identifier',
+          '==',
+          this.route.snapshot.paramMap.get('billId')
+        )
+      )
+      .valueChanges();
+    this.legislationCollection$.subscribe((data) => {
+      if (data.length > 1) throw new Error('too many results');
+      else this.bill = data[0];
+    });
   }
 
+  ngOnInit(): void {}
 }
