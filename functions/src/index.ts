960c1b770abb3b372e47cff2058099294553a662
JPStroud <jpstroud@gmail.com>
2021-03-08 22:15:34 -0500

    api call

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as ny from './ny/functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const test = functions.https.onRequest((request, response) => {
+export const test = functions.https.onRequest(async (request, response) => {
   functions.logger.info(`test run started @ ${new Date()}`, {
     structuredData: true,
   });
+
+  await ny.getUpdatedMembers();
+
   response.send(`test run completed @ ${new Date()}`);
 });
