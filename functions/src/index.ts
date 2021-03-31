8c26c0c19767a68657bac9674997ca42920d53a4
JPStroud <jpstroud@gmail.com>
2021-03-31 18:22:52 -0400

    test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 import { Legislation } from './app/legislation';
 import { Sponsorship } from './app/shared';
 // import * as ny from './ny/functions';
-import * as fn from './ny/functions2';
+import * as ny from './ny/functions2';
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -17,32 +17,26 @@ export const test2 = functions.https.onRequest(async (request, response) => {
 });
 
 export const test = functions.https.onRequest(async (request, response) => {
-  // try {
-  //   const colRef = db.collection('legislation');
-  //   const snapshot = await colRef.get();
-  //   if (snapshot.empty) throw new Error(`no bills to update!`);
-  //   else {
-  //     snapshot.forEach(async (doc) => {
-  //       const bill: Legislation = doc.data() as Legislation;
-  //       const billData: Legislation = await ny.getBillData(bill);
+  try {
+    const colRef = db.collection('legislation');
+    const snapshot = await colRef.get();
+    if (snapshot.empty) throw new Error(`no bills to update!`);
+    else {
+      snapshot.forEach(async (doc) => {
+        console.log(await getBillUpdates(doc.data() as Legislation));
+      });
+    }
+  } catch (error) {
+    functions.logger.error(error);
+    response.send('Run completed with errors!');
+  }
 
-  //       const sponsorship: Sponsorship = {
-  //         name: bill.name,
-  //         identifier: bill.identifier,
-  //         version: billData.version,
-  //         date: '',
-  //       };
-
-  //       await batchSponsorshipUpdates(billData.sponsorships, sponsorship);
-  //       await colRef.doc(doc.id).set(billData);
-  //     });
-  //   }
-  // } catch (error) {
-  //   functions.logger.error(error);
-  // }
-
-  response.send(`test run completed @ ${new Date()}`);
+  response.send('Run completed successfully');
 });
+
+function getBillUpdates(bill: Legislation) {
+  return ny.getBillSponsorshipUpdates(bill.identifier);
+}
 
 // function batchSponsorshipUpdates(
 //   sponsorList: Sponsorship[],
